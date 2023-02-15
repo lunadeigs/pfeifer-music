2703cfdf37cb4f5dc7e81ace8c1ea898062b9a46
lunadeigs <lunadeighan@gmail.com>
2023-02-14 21:56:26 -0600

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* External dependencies */
 import React from 'react';
-import { HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 /* Internal dependencies */
 import './App.css';
