8ae616d4336302d9956592d30b15362f87c6288c
lunadeigs <lunadeighan@gmail.com>
2023-03-04 14:36:59 -0600

    Hash router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// import { BrowserRouter as Router } from 'react-router-dom'; // LOCAL
+import { HashRouter as Router } from 'react-router-dom'; // Deployed to Github
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
